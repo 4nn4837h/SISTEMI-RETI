97b2490fc54f8a611edfcde5f74f0ad986cef6cb
4nn4837h <81015568+4nn4837h@users.noreply.github.com>
2021-11-04 23:05:47 +0100

    Add files via upload

---
+++ es/esercizio_008_dizionario.py
@@ -0,0 +1,5 @@
+""" dizionari: contenitori particolari: coppie di valori: chiave - valore"""
+dizionario = {"to get":"prendere", "hello":"ciao", 
+                "print":"stampa"}
+# non ci sono gli indici
+print(dizionario["hello"]) # stampa ciao

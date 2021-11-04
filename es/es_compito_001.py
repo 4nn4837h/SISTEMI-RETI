94ae1ef800575fad15328d0f035f7931ee2690c2
4nn4837h <81015568+4nn4837h@users.noreply.github.com>
2021-11-04 23:14:28 +0100

    Add files via upload

---
+++ es/es_compito_001.py
@@ -0,0 +1,9 @@
+"""ESERCIZIO 1 IN PYTHON:
+ 4 variabili: int, String, floating point e booleano;
+ stamparle con la funzione type"""
+
+intera = 1
+stringa = "ciao"
+floatingpoint = 1.1
+booleana = True
+print(type(intera), print([intera]), type(stringa), print([stringa]), type(floatingpoint), print([floatingpoint]), type(booleana), print([booleana]))
